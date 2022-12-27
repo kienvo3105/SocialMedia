a7b757d22f0cf298884afa1ee1d6d2895196aaf0
kienvo3105 <hvtrungkien@gmail.com>
2022-12-27 17:07:45 +0700

    Create folder class and component

--- Social-Media/Program.cs
+++ Social-Media/Program.cs
@@ -16,7 +16,7 @@ namespace Social_Media
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
